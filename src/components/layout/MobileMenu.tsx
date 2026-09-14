cda99f01adcda6038ba0a4cbc492daa7a33e5dfc
Haseeb <haseebbasit2707@gmail.com>
2026-09-15 01:17:48 +0500

    DROPDOWN

--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -66,7 +66,7 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
           left: 0,
           right: 0,
           bottom: 0,
-          background: "rgba(0,0,0,0.6)",
+          background: "rgba(0,0,0,0.25)",
           backdropFilter: "blur(4px)",
           WebkitBackdropFilter: "blur(4px)",
           zIndex: 9999,
